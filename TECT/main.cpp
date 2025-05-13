474b7a93c98fb04881067163e7ea5580e70653ca
DaDMraZ <k79176241840@gmail.com>
2025-05-13 10:26:41 +0400

    Add files via upload

---
+++ TECT/main.cpp
@@ -0,0 +1,64 @@
+
+#include "TXLib.h"
+
+bool click(int x)
+{
+ return( txMouseButtons()==1
+ && txMouseX()> x && txMouseX()<x+300 &&
+ txMouseY()>510 && tx MouseY()<570 );
+
+
+
+
+
+}
+
+
+
+
+
+
+
+int main()
+    {
+    txCreateWindow (1101, 528);
+    txTextCursor (false);
+
+    HDC kingS = txLoadImage("КАРТИНКИ/kingS.bmb");
+
+    txSetColor (TX_WHITE);
+    txSetFillColor (TX_TRANSPARENT);
+    txRectangle (850, 20, 1040, 50); //номер вопрос
+    txRectangle (280, 40, 800, 110);  // вопрос
+    txRectangle (50, 150, 340, 400);// картинка 1
+    txRectangle (370, 150, 680, 400);//картинка 2
+    txRectangle (700, 150, 980, 400);//картинка 3
+    txRectangle (50, 420, 340, 470);// ответ 1
+    txRectangle (370, 420, 680, 470);//ответ 2
+    txRectangle (700, 420, 980, 470);//ответ 3
+
+    txSetColor (TX_LIGHTGREEN);
+    txSelectFont ("Times", 40);
+    txSetTextAlign (TA_CENTER);
+
+
+
+
+    txDrawText (850, 20, 1040, 50, "Вопрос№");
+
+    txDrawText (280, 40, 800, 110, "Вопрос1241213131313");
+
+    txBitBlt ( 50, 150, 340, 400, kingS);
+    //txBitBlt (850, 20, 1040, 50, );
+   // txBitBlt (850, 20, 1040, 50, "Вопрос№");
+
+    txDrawText (50, 420, 340, 470, "Ответ 1");
+    txDrawText (370, 420, 680, 470, "Ответ 2");
+    txDrawText (700, 420, 980, 470, "Ответ 3");
+
+
+
+
+    return 0;
+    }
+
